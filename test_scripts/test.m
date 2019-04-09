e2f0d590b3ed193f11a6ec3f52bf92d159deefb7
Eric Lei <el536@cornell.edu>
2019-04-09 15:20:45 -0400

    matlab script compares complexity scaling

--- test_scripts/test.m
+++ test_scripts/test.m
@@ -1,13 +1,21 @@
 clear
 
-N = 8;
+N = 256;
 t = (0:N-1)';
 x = sin(t);
 
 [A,B] = fftc_lib.singleBfly(3+2j, 3+2j, 3, N);
 
-xout = fftc_lib.fftc(x, N)
-xoutdft = fftc_lib.slow_dft(x,N)
-xoutfft = fft(x)
+tic
+for i=1:500
+    xout = fftc_lib.fftc(x, N);
+end
+timeFFT = toc
+tic
+for i=1:500
+    xoutdft = fftc_lib.slow_dft(x,N);
+end
+timeDFT = toc
+xoutfft = fft(x);
 
 error = mean(abs(xout - xoutfft))
